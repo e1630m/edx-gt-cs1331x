7819d25f79de74ebb25f69e855c0b575bbeb5924
e1630m <e1630m@gmail.com>
2021-09-26 08:18:00 +0000

    Fix hw01/Bad1-3 (Section 3)

--- hw01/Bad2.java
+++ hw01/Bad2.java
@@ -1,7 +1,7 @@
 public class Bad2 {
     public static void main(String[] args) {
         int a = 1331;
-        int b = 0;
+        int b = 1;
         System.out.println("Welcome to \nCS 1331!");
         int c = a / b;
         System.out.println("c is equal to: " + c);
